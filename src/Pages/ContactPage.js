4473a4d52445197b23dc657cd120eaa5ec3a7e46
Rafał <heller.rafal@gmail.com>
2019-06-14 17:25:57 +0200

    add Prompt component in ContactPage

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,33 +1,50 @@
 import React, {Component} from 'react';
 import '../scss/ContactPage.scss'
+import {Prompt} from "react-router-dom"
 
 class ContactPage extends Component {
     state = {
-        value:""
+        value: "",
+        isEmpty: true
     }
     handleSubmit = (e) => {
         e.preventDefault()
         this.setState({
-            value: ""
+            value: "",
+            isEmpty: true
         })
     }
-handleChange = (e) => {
-        this.setState({
-            value: e.target.value
+    handleChange = (e) => {
+        if (e.target.value > 0) {
+            this.setState({
+                value: e.target.value,
+                isEmpty: false
+            })
+        } else {
+            this.setState({
+                value: e.target.value,
+            isEmpty: false
         })
-}
+        }
+    }
+
+
     render() {
         return (
-         <div className="contact">
-             <form onSubmit={this.handleSubmit}>
-                 <h3>Napisz do nas</h3>
-                 <textarea value={this.state.value}
-                           cols="30"
-                           rows="10"
-                 onChange={this.handleChange}></textarea>
-                 <button >Wyślij</button>
-             </form>
-         </div>
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea value={this.state.value}
+                              cols="30"
+                              rows="10"
+                              onChange={this.handleChange}></textarea>
+                    <button>Wyślij</button>
+                </form>
+                <Prompt
+                    when={!this.state.isEmpty}
+                    message='Masz niewypełniony formularz. Czy napewno chcesz opuścić tę stronę ?'
+                />
+            </div>
         );
     }
 }
